825eb8560b07b3f61853eacec1208b8a6a872e97
Al-Shouha Modafar <modafar.al-shouha@nokia.com>
2023-02-15 21:27:27 +0100

    add argparser

--- IterIC.py
+++ IterIC.py
@@ -14,21 +14,32 @@ from IC_explaination import Explainer
 
 import argparse
 
-
 parser = argparse.ArgumentParser()
-parser.add_argument('--name', type=str, required=True)
-
+parser.add_argument('--dataset', default='flickr8k', type=str, required=False, help='image captioning model dataset: coco or flickr8k')
+parser.add_argument('--seg_dataset', default='coco', type=str, required=False, help='instance segmentation model dataset: coco or lvis')
+parser.add_argument('--seg_backbone', default='r50', type=str, required=False, help='instance segmentation model backbone: r50 or x101')
+parser.add_argument('--weights_dir', default='./weights', type=str, required=False, help='directory path for the image captioning models weights')
+parser.add_argument('--algorithm', default='quickshift', type=str, required=False, help='low level segmentation algorithm: quickshift, felzenszwalb or slic')
+parser.add_argument('--kernel_size', default=20, type=int, required=False, help='low level segmentation kernel size (int)')
+parser.add_argument('--max_dist', default=200, type=int, required=False, help='low level segmentation maximum distance (int)')
+parser.add_argument('--ratio', default=0.2, type=float, required=False, help='low level segmentation ratio (float)')
 
 
 if __name__=='__main__':
 
     args = parser.parse_args()
-    print('Hello,', args.name)
+
+    CaptioningModel = IC(dataset=args.dataset, weights_dir=args.weights_dir)
+    InstanceSegModel = InstanceSegmenter(dataset=args.seg_dataset, backbone=args.seg_backbone)
+    RandomSegModel = RandomSegmenter(args.algorithm, kernel_size=args.kernel_size,
+                                     max_dist=args.max_dist, ratio=args.ratio,
+                                     random_seed=42)
+    ExplainerModel = Explainer(captioner=CaptioningModel, instance_seg=InstanceSegModel, random_seg=RandomSegModel)
 
     # dataset = 'flickr8k'
     # instance_seg_dataset = 'coco' # coco or lvis
     # instance_seg_backbone = 'r50' # r50 or x101
-    # CaptioningModel = IC(dataset=dataset, weights_dir='./XAI/Iterative/weights')
+    # CaptioningModel = IC(dataset=dataset, weights_dir='./weights')
     # InstanceSegModel = InstanceSegmenter(dataset=instance_seg_dataset, backbone=instance_seg_backbone)
     # RandomSegModel = RandomSegmenter('quickshift', kernel_size=20,
     #                                   max_dist=200, ratio=0.2,
